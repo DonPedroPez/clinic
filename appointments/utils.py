1dbb52565075cec0551cc627d6a8649c7e1f2379
Piotr Rybarczyk <piotr.rybarczyk@student.put.poznan.pl>
2015-10-18 19:26:50 +0200

    Added suggested appointments util.

--- appointments/utils.py
+++ appointments/utils.py
@@ -1,8 +1,22 @@
 from datetime import datetime, timedelta
 
-from .models import Duty
+from .models import Duty, Appointment
 
 
-def get_suggested_appointments_for_date(doctor, clinic, date):
+def get_suggested_appointments(doctor, clinic, date):
     duties = Duty.objects.filter(doctor=doctor, clinic=clinic,
                                  weekday=date.weekday())
+    if not duties:
+        return None
+    appointments = Appointment.objects.filter(doctor=doctor, clinic=clinic,
+                                              date=date)
+    suggested = []
+    for duty in duties:
+        proposition = duty.start
+        while proposition < duty.end:
+            if not appointments.filter(start__lte=proposition,
+                                       end__gte=proposition):
+                suggested.append(proposition)
+            proposition = (datetime.combine(date, proposition)
+                           + timedelta(minutes=30)).time()
+    return suggested
